a3317ce1b40d88eadd62deffe8e750e371bb5755
Muhammad Arslan <iqbalarslan009@gmail.com>
2025-07-09 13:30:50 +0500

    build summrizer user interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [textToSummarize, setTextToSummarize] = useState("");
+
+  function handleTextChange(text) {
+    setTextToSummarize(text);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <main>
+        <p>Enter text you want to Summarize</p>
+        <textarea
+          value={textToSummarize}
+          rows={20}
+          cols={70}
+          placeholder="Paste or type your text here..."
+          onChange={(e) => handleTextChange(e.target.value)}
+        />
+        <br />
+        <button>Summarize</button>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
